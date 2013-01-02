14ac501835dd0af1a41a0b52c1d1b365382cc698
Jarrod Overson <joverson@onehealth.com>
2013-01-02 15:10:14 -0800

    updated example script

---
+++ update.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+TMPDIR=/tmp/plato
+
+CMD=./bin/plato
+
+unset REFETCH
+
+if [ $1 ]
+then
+REFETCH=true
+fi
+
+
+if [ $REFETCH ] && [ -d $TMPDIR ]
+then
+  echo "Removing $TMPDIR"
+  rm -rf $TMPDIR
+fi
+
+if [ -d examples ]
+then
+  echo "Removing old examples"
+  rm -rf examples
+fi
+
+if [ $REFETCH ]
+then
+git clone --depth 1 git://github.com/emberjs/ember.js.git $TMPDIR/ember
+git clone --depth 1 git://github.com/jquery/jquery.git $TMPDIR/jquery
+git clone --depth 1 git://github.com/gruntjs/grunt.git $TMPDIR/grunt
+git clone --depth 1 https://github.com/marionettejs/backbone.marionette.git $TMPDIR/marionette
+git clone --depth 1 git://github.com/mishoo/UglifyJS2.git $TMPDIR/uglify
+fi
+
+$CMD -r -d examples/ember/ $TMPDIR/ember/packages/ember-runtime/lib/*
+$CMD -r -d examples/jquery/ $TMPDIR/jquery/src/*
+$CMD -r -d examples/grunt/ $TMPDIR/grunt/lib/*
+$CMD -d examples/marionette/ $TMPDIR/marionette/src/*.js
+$CMD -r -d examples/uglify/ $TMPDIR/uglify/lib/*.js $TMPDIR/uglify/bin/*
+
