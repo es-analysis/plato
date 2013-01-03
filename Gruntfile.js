8d689597524d1481f8a3728ca93f35091a95ab53
Jarrod Overson <joverson@onehealth.com>
2013-01-02 16:46:37 -0800

    bundled js, perf tweaks, style tweaks

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,26 @@ module.exports = function(grunt) {
         src: ['test/**/*.js']
       },
     },
+    uglify: {
+      'assets' : {
+        files : {
+          'lib/assets/scripts/bundles/core-bundle.js' : [
+            'lib/assets/scripts/vendor/jquery-1.8.3.min.js',
+            'lib/assets/scripts/vendor/lodash.min.js',
+            'lib/assets/scripts/vendor/raphael-min.js',
+            'lib/assets/scripts/vendor/morris.min.js',
+            'lib/assets/scripts/vendor/jquery.fittext.js',
+            'lib/assets/scripts/vendor/bootstrap-tooltip.js',
+            'lib/assets/scripts/vendor/bootstrap-popover.js'
+          ],
+          'lib/assets/scripts/bundles/codemirror.js' : [
+            'lib/assets/scripts/vendor/codemirror/codemirror.js',
+            'lib/assets/scripts/vendor/codemirror/javascript.js',
+            'lib/assets/scripts/vendor/codemirror/util/searchcursor.js'
+          ]
+        }
+      },
+    },
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
@@ -39,7 +59,9 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  grunt.registerTask('optimize', ['uglify']);
   // Default task.
   grunt.registerTask('default', ['jshint', 'nodeunit']);
 
