6c20bd144eec0f3a04125005dc5d072d1178f5bd
Jarrod Overson <joverson@onehealth.com>
2013-01-02 19:49:52 -0800

    added esprima

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 TMPDIR=/tmp/plato
 
-CMD=./bin/plato
+CMD=plato
 
 unset REFETCH
 
@@ -31,6 +31,7 @@ git clone --depth 1 git://github.com/jquery/jquery.git $TMPDIR/jquery
 git clone --depth 1 git://github.com/gruntjs/grunt.git $TMPDIR/grunt
 git clone --depth 1 https://github.com/marionettejs/backbone.marionette.git $TMPDIR/marionette
 git clone --depth 1 git://github.com/mishoo/UglifyJS2.git $TMPDIR/uglify
+git clone --depth 1 git://github.com/ariya/esprima.git $TMPDIR/esprima
 fi
 
 $CMD -r -d examples/ember/ $TMPDIR/ember/packages/ember-runtime/lib/*
@@ -38,4 +39,4 @@ $CMD -r -d examples/jquery/ $TMPDIR/jquery/src/*
 $CMD -r -d examples/grunt/ $TMPDIR/grunt/lib/*
 $CMD -d examples/marionette/ $TMPDIR/marionette/src/*.js
 $CMD -r -d examples/uglify/ $TMPDIR/uglify/lib/*.js $TMPDIR/uglify/bin/*
-
+$CMD -d examples/esprima $TMPDIR/esprima/esprima.js
