5baeeb9bf0e90c91a43e4286240738b9819e1485
Jarrod Overson <joverson@onehealth.com>
2013-01-06 00:44:25 -0800

    added unit tests

--- test/plato_test.js
+++ test/plato_test.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var util = require('../lib/util');
+var util = require('../lib/util'),
+    plato = require('../lib/plato');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -44,5 +45,54 @@ exports['awesome'] = {
     test.equal(util.findCommonBase(files), '', 'should nto find common prefix for one file');
 
     test.done();
+  },
+  'test empty file' : function(test) {
+    test.expect(1);
+
+    var files = [
+      'test/fixtures/a.js',
+      'test/fixtures/b.js',
+      'test/fixtures/empty.js'
+    ];
+
+    plato.inspect(files, null, {}, function(reports){
+      test.equal(reports.length, 2, 'Should not attempt to report on empty files');
+      test.done();
+    });
+  },
+  'test report structure' : function(test) {
+    test.expect(4);
+
+    var files = [
+      'test/fixtures/a.js',
+      'test/fixtures/b.js'
+    ];
+
+    plato.inspect(files, null, {}, function(reports){
+      reports.forEach(function(report){
+        test.ok(report.complexity, 'Should contain a complexity report');
+        test.ok(report.jshint, 'Should contain a jshint report');
+      });
+      test.done();
+    });
+  },
+  'test overview report structure' : function(test) {
+
+    var files = [
+      'test/fixtures/a.js',
+      'test/fixtures/b.js'
+    ];
+
+    test.expect((files.length * 2) + 1);
+
+    plato.inspect(files, null, {}, function(reports){
+      var overview = plato.getOverviewReport(reports);
+      test.ok(overview.summary.total.sloc > 0, 'Should contain total sloc');
+      test.ok(overview.summary.total.maintainability > 0, 'Should contain total maintainability');
+      test.ok(overview.summary.average.sloc > 0, 'Should contain average sloc');
+      test.ok(overview.summary.average.maintainability > 0, 'Should contain average maintainability');
+      test.equal(overview.reports.length, files.length,'Should contain right number of reports');
+      test.done();
+    });
   },
 };
