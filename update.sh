b3263b5948c17f5241eedbafd6f5a964de172e50
Jarrod Overson <joverson@onehealth.com>
2013-03-27 11:05:51 -0700

    added historical generation to update script

--- update.sh
+++ update.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+ORIGWD=$PWD
+
 TMPDIR=/tmp/plato
 
-CMD=plato
+CMD=$PWD/bin/plato
+
+echo Original directory : $ORIGWD
+echo Temporary root directory : $TMPDIR
+echo Command : $CMD
 
 unset REFETCH
 
 if [ $1 ]
 then
 REFETCH=true
+fi
+
+if [ -d examples ]
+then
+ rm -rf examples
 fi
 
 
@@ -19,27 +30,35 @@ then
     echo "Removing $TMPDIR"
     rm -rf $TMPDIR
   fi
-  git clone --depth 1 git://github.com/jquery/jquery.git $TMPDIR/jquery
-  git clone --depth 1 git://github.com/gruntjs/grunt.git $TMPDIR/grunt
-  git clone --depth 1 https://github.com/marionettejs/backbone.marionette.git $TMPDIR/marionette
+  git clone git://github.com/jquery/jquery.git $TMPDIR/jquery
+  git clone git://github.com/gruntjs/grunt.git $TMPDIR/grunt
+  git clone https://github.com/marionettejs/backbone.marionette.git $TMPDIR/marionette
 fi
 
-OLDPWD=PWD
+function runCommand() {
+  echo $1
+  $1
+}
 
-for days_ago in 10 20 30 60; do
-   DATE=$(date -j -f "%a %b %d %T %Z %Y" "`date -v -20d`" "+%s")
+for DAYS_AGO in 60 30 20 10 0; do
+  DATE=$(date -j -f "%a %b %d %T %Z %Y" "`date -v -${DAYS_AGO}d`" "+%s")
 
-   cd $TMPDIR/jquery
-   git co -f $(git rev-list -n 1 --before="$date" master);
-   $CMD -D $DATE -r -d $OLDPWD/examples/jquery -r src;
+  echo Using date $DATE / $(date -j -f "%s" "$DATE" "+%a %b %d %T %Z %Y")
 
-   cd $TMPDIR/marionette
-   git co -f $(git rev-list -n 1 --before="$date" master);
-   $CMD -D $DATE -d $OLDPWD/examples/marionette -r src/*.js;
+  cd $TMPDIR/jquery &&
+    echo In $PWD &&
+    git co -f $(git rev-list -n 1 --before="$DATE" master) &&
+    runCommand "$CMD -D $DATE -r -d $ORIGWD/examples/jquery -x 'exports.js|tro.js' src";
 
-   cd $TMPDIR/grunt
-   git co -f $(git rev-list -n 1 --before="$date" master);
-   $CMD -D $DATE -r -d $OLDPWD/examples/grunt-r src;
+  cd $TMPDIR/marionette &&
+    echo In $PWD &&
+    git co -f $(git rev-list -n 1 --before="$DATE" master) &&
+    runCommand "$CMD -D $DATE -d $ORIGWD/examples/marionette src/*.js";
+
+  cd $TMPDIR/grunt &&
+    echo In $PWD &&
+    git co -f $(git rev-list -n 1 --before="$DATE" master) &&
+    runCommand "$CMD -D $DATE -r -d $ORIGWD/examples/grunt lib";
 done
 
-cd $OLDPWD
+cd $ORIGWD
