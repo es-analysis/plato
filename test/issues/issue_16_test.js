b11e18edce39e9ea527ec877f179304a89665a7b
Pedro Nauck <pedronauck@gmail.com>
2014-03-25 23:18:28 -0300

    fix brand link in header

--- test/issues/issue_16_test.js
+++ test/issues/issue_16_test.js
@@ -2,16 +2,16 @@
 
 // [Issue 16][gh]: Support for JSHint style globals
 // -----------------------------------------------------------------------------
-// 
+//
 // This issue is resolved by upgrading to jshint version 1.1. There was a bug
 // that required that there by no space in the global comment:
 //
 //     /*global $ */
-// 
+//
 // In their [1.0.0 RC1][jshint], they've patched this problem. This test should catch any
 // regression to this behavior.
 
-// [gh]: https://github.com/jsoverson/plato/issues/16
+// [gh]: https://github.com/es-analysis/plato/issues/16
 // [jshint]: http://www.jshint.com/blog/2012-12-29/1-0-0-rc1/
 
 var fs = require('fs-extra'),
@@ -23,7 +23,7 @@ exports['issue_16'] = {
   },
 
   'Respect global comment': function(test) {
-  
+
     var file = "test/fixtures/issue_16.js",
         source = fs.readFileSync(file).toString().trim(),
         config = {},
